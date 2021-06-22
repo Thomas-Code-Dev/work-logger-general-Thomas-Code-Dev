fc55ed69ab022e898a5d1f10883076273d601234
mtamas <gpdcaribbeansea@gmail.com>
2021-06-22 07:23:47 +0200

    feat: intruducing month parameter

--- alice.sh
+++ alice.sh
@@ -2,11 +2,18 @@
 
 sum_working_hours() {
   count=0
+  month=$1
+  if [ -z "$month" ]; then
+    ${month:=january}
+  fi
+  file="logs/alice/$month.csv"
+  if [ ! -f "${file}" ]; then
+    echo "Error, the log for ${month^} does not exist" && exit 1
+  fi
   while IFS=, read -r date day_of_week hours_worked; do
     [[ "$hours_worked" != "hours worked" ]] && ((count = count + $hours_worked))
-  done < logs/alice/january.csv
+  done <$file
   echo "${count}"
 }
 
-sum_working_hours
-
+sum_working_hours $1
--- bob.sh
+++ bob.sh
@@ -2,10 +2,19 @@
 
 sum_working_hours() {
   count=0
+  month=$1
+  if [ -z "$month" ]; then
+    ${month:=january}
+  fi
+  file="logs/bob/$month.csv"
+  if [ ! -f "${file}" ]; then
+    echo "Error, the log for ${month^} does not exist" && exit 1
+  fi
   while IFS=, read -r date day_of_week hours_worked; do
     [[ "$hours_worked" != "hours worked" ]] && ((count = count + $hours_worked))
-  done < logs/bob/january.csv
+  done <"$file"
+
   echo "${count}"
 }
 
-sum_working_hours
+sum_working_hours "$1"
