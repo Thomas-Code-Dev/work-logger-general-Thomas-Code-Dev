7e7c44903eb0629765172ae9e519211ad481d8d6
mtamas <gpdcaribbeansea@gmail.com>
2021-06-22 13:57:06 +0200

    refactor: shared.sh and report.sh

--- report.sh
+++ report.sh
@@ -4,18 +4,34 @@ sum_work_hours() {
   count=0
   name=${1,}
   month=${2,}
+  apply_default_month
+  apply_default_employee
+  check_file_existance
+  calculate_work_hours
+}
+
+apply_default_month() {
   if [ -z "$month" ]; then
     ${month:=january}
   fi
+}
+
+apply_default_employee() {
   if [ -z "$name" ]; then
     ${name:=alice}
   fi
+}
+
+check_file_existance() {
   file="logs/$name/$month.csv"
   if [ ! -f "${file}" ]; then
     echo "Error, the log for ${name^} regarding ${month^} does not exist" && exit 1
   fi
-  while IFS=, read -r date day_of_week hours_worked; do
-    [[ "$hours_worked" != "hours worked" ]] && ((count = count + $hours_worked))
+}
+
+calculate_work_hours() {
+    while IFS=, read -r date day_of_week hours_worked; do
+    [[ "$hours_worked" != "hours worked" ]] && ((count = count + hours_worked))
   done <"$file"
   echo "${count}"
 }
